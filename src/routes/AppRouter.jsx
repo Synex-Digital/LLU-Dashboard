1e0ed9c19f28f903ddf13b4cc19b6afed2d9fddf
Tanvir Mahmud Tamim <tanvirmahmudtamim59@gmail.com>
2024-09-10 23:07:43 +0600

    Facility View design complete

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,8 +1,8 @@
 import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider,
+    createBrowserRouter,
+    createRoutesFromElements,
+    Route,
+    RouterProvider,
 } from "react-router-dom";
 import { Helmet } from "react-helmet-async";
 import { routes } from "./Routers";
@@ -23,213 +23,277 @@ import Notifications from "../pages/Notifications";
 import AthleteRequest from "../pages/AthleteRequest";
 import CreateSession from "../pages/CreateSession";
 import Profile from "../pages/Profile";
+import FacilityView from "../pages/FacilityView";
 
 const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route>
-      <Route
-        path={routes.getStarted.path}
-        element={
-          <>
-            <Helmet>
-              <title>{routes.getStarted.title}</title>
-              <meta name="description" content="LLU Description" />
-            </Helmet>
-            <GetStarted />
-          </>
-        }
-      ></Route>
-      <Route
-        path={routes.signup.path}
-        element={
-          <>
-            <Helmet>
-              <title>{routes.signup.title}</title>
-              <meta name="description" content="LLU Description" />
-            </Helmet>
-            <SignUp />
-          </>
-        }
-      ></Route>
-      <Route
-        path={routes.login.path}
-        element={
-          <>
-            <Helmet>
-              <title>{routes.login.title}</title>
-              <meta name="description" content="LLU Description" />
-            </Helmet>
-            <Login />
-          </>
-        }
-      ></Route>
-      <Route
-        path={routes.forgotPassword.path}
-        element={
-          <>
-            <Helmet>
-              <title>{routes.forgotPassword.title}</title>
-              <meta name="description" content="LLU Description" />
-            </Helmet>
-            <ForgotPassword />
-          </>
-        }
-      ></Route>
-      <Route
-        path={routes.verifiedOtp.path}
-        element={
-          <>
-            <Helmet>
-              <title>{routes.verifiedOtp.title}</title>
-              <meta name="description" content="LLU Description" />
-            </Helmet>
-            <VerifiedOtp />
-          </>
-        }
-      ></Route>
-      <Route
-        path={routes.setNewPassword.path}
-        element={
-          <>
-            <Helmet>
-              <title>{routes.setNewPassword.title}</title>
-              <meta name="description" content="LLU Description" />
-            </Helmet>
-            <SetNewPassword />
-          </>
-        }
-      ></Route>
-      <Route
-        path={routes.notFound.path}
-        element={
-          <>
-            <Helmet>
-              <title>{routes.notFound.title}</title>
-              <meta name="description" content="LLU Description" />
-            </Helmet>
-            <NotFoundPage />
-          </>
-        }
-      />
-      <Route
-        path={routes.selectUser.path}
-        element={
-          <>
-            <Helmet>
-              <title>{routes.selectUser.title}</title>
-              <meta name="description" content="LLU Description" />
-            </Helmet>
-            <SelectUser />
-          </>
-        }
-      />
-      <Route
-        path={routes.home.path}
-        element={
-          <>
-            <Helmet>
-              <title>{routes.home.title}</title>
-              <meta name="description" content="LLU Description" />
-            </Helmet>
-            <RotLayOut />
-          </>
-        }
-      >
-        <Route
-          path={routes.dashboard.path}
-          element={
-            <>
-              <Helmet>
-                <title>{routes.dashboard.title}</title>
-                <meta name="description" content="LLU Description" />
-              </Helmet>
-              <Home />
-            </>
-          }
-        />
-        <Route
-          path={routes.session.path}
-          element={
-            <>
-              <Helmet>
-                <title>{routes.session.title}</title>
-                <meta name="description" content="LLU Description" />
-              </Helmet>
-              <Session />
-            </>
-          }
-        />
-        <Route
-          path={routes.sessionDetails.path}
-          element={
-            <>
-              <Helmet>
-                <title>{routes.sessionDetails.title}</title>
-                <meta name="description" content="LLU Description" />
-              </Helmet>
-              <SessionDetails />
-            </>
-          }
-        />
-        <Route
-          path={routes.notifications.path}
-          element={
-            <>
-              <Helmet>
-                <title>{routes.notifications.title}</title>
-                <meta name="description" content="LLU Description" />
-              </Helmet>
-              <Notifications />
-            </>
-          }
-        />
-        <Route
-          path={routes.athleteRequest.path}
-          element={
-            <>
-              <Helmet>
-                <title>{routes.athleteRequest.title}</title>
-                <meta name="description" content="LLU Description" />
-              </Helmet>
-              <AthleteRequest />
-            </>
-          }
-        />
-        <Route
-          path={routes.createSession.path}
-          element={
-            <>
-              <Helmet>
-                <title>{routes.createSession.title}</title>
-                <meta name="description" content="LLU Description" />
-              </Helmet>
-              <CreateSession />
-            </>
-          }
-        />
-        <Route
-          path={routes.profile.path}
-          element={
-            <>
-              <Helmet>
-                <title>{routes.profile.title}</title>
-                <meta name="description" content="LLU Description" />
-              </Helmet>
-              <Profile />
-            </>
-          }
-        />
-      </Route>
-    </Route>,
-  ),
+    createRoutesFromElements(
+        <Route>
+            <Route
+                path={routes.getStarted.path}
+                element={
+                    <>
+                        <Helmet>
+                            <title>{routes.getStarted.title}</title>
+                            <meta
+                                name="description"
+                                content="LLU Description"
+                            />
+                        </Helmet>
+                        <GetStarted />
+                    </>
+                }
+            ></Route>
+            <Route
+                path={routes.signup.path}
+                element={
+                    <>
+                        <Helmet>
+                            <title>{routes.signup.title}</title>
+                            <meta
+                                name="description"
+                                content="LLU Description"
+                            />
+                        </Helmet>
+                        <SignUp />
+                    </>
+                }
+            ></Route>
+            <Route
+                path={routes.login.path}
+                element={
+                    <>
+                        <Helmet>
+                            <title>{routes.login.title}</title>
+                            <meta
+                                name="description"
+                                content="LLU Description"
+                            />
+                        </Helmet>
+                        <Login />
+                    </>
+                }
+            ></Route>
+            <Route
+                path={routes.forgotPassword.path}
+                element={
+                    <>
+                        <Helmet>
+                            <title>{routes.forgotPassword.title}</title>
+                            <meta
+                                name="description"
+                                content="LLU Description"
+                            />
+                        </Helmet>
+                        <ForgotPassword />
+                    </>
+                }
+            ></Route>
+            <Route
+                path={routes.verifiedOtp.path}
+                element={
+                    <>
+                        <Helmet>
+                            <title>{routes.verifiedOtp.title}</title>
+                            <meta
+                                name="description"
+                                content="LLU Description"
+                            />
+                        </Helmet>
+                        <VerifiedOtp />
+                    </>
+                }
+            ></Route>
+            <Route
+                path={routes.setNewPassword.path}
+                element={
+                    <>
+                        <Helmet>
+                            <title>{routes.setNewPassword.title}</title>
+                            <meta
+                                name="description"
+                                content="LLU Description"
+                            />
+                        </Helmet>
+                        <SetNewPassword />
+                    </>
+                }
+            ></Route>
+            <Route
+                path={routes.notFound.path}
+                element={
+                    <>
+                        <Helmet>
+                            <title>{routes.notFound.title}</title>
+                            <meta
+                                name="description"
+                                content="LLU Description"
+                            />
+                        </Helmet>
+                        <NotFoundPage />
+                    </>
+                }
+            />
+            <Route
+                path={routes.selectUser.path}
+                element={
+                    <>
+                        <Helmet>
+                            <title>{routes.selectUser.title}</title>
+                            <meta
+                                name="description"
+                                content="LLU Description"
+                            />
+                        </Helmet>
+                        <SelectUser />
+                    </>
+                }
+            />
+            <Route
+                path={routes.home.path}
+                element={
+                    <>
+                        <Helmet>
+                            <title>{routes.home.title}</title>
+                            <meta
+                                name="description"
+                                content="LLU Description"
+                            />
+                        </Helmet>
+                        <RotLayOut />
+                    </>
+                }
+            >
+                <Route
+                    path={routes.dashboard.path}
+                    element={
+                        <>
+                            <Helmet>
+                                <title>{routes.dashboard.title}</title>
+                                <meta
+                                    name="description"
+                                    content="LLU Description"
+                                />
+                            </Helmet>
+                            <Home />
+                        </>
+                    }
+                />
+                <Route
+                    path={routes.session.path}
+                    element={
+                        <>
+                            <Helmet>
+                                <title>{routes.session.title}</title>
+                                <meta
+                                    name="description"
+                                    content="LLU Description"
+                                />
+                            </Helmet>
+                            <Session />
+                        </>
+                    }
+                />
+                <Route
+                    path={routes.sessionDetails.path}
+                    element={
+                        <>
+                            <Helmet>
+                                <title>{routes.sessionDetails.title}</title>
+                                <meta
+                                    name="description"
+                                    content="LLU Description"
+                                />
+                            </Helmet>
+                            <SessionDetails />
+                        </>
+                    }
+                />
+                <Route
+                    path={routes.notifications.path}
+                    element={
+                        <>
+                            <Helmet>
+                                <title>{routes.notifications.title}</title>
+                                <meta
+                                    name="description"
+                                    content="LLU Description"
+                                />
+                            </Helmet>
+                            <Notifications />
+                        </>
+                    }
+                />
+                <Route
+                    path={routes.athleteRequest.path}
+                    element={
+                        <>
+                            <Helmet>
+                                <title>{routes.athleteRequest.title}</title>
+                                <meta
+                                    name="description"
+                                    content="LLU Description"
+                                />
+                            </Helmet>
+                            <AthleteRequest />
+                        </>
+                    }
+                />
+                <Route
+                    path={routes.createSession.path}
+                    element={
+                        <>
+                            <Helmet>
+                                <title>{routes.createSession.title}</title>
+                                <meta
+                                    name="description"
+                                    content="LLU Description"
+                                />
+                            </Helmet>
+                            <CreateSession />
+                        </>
+                    }
+                />
+                <Route
+                    path={routes.profile.path}
+                    element={
+                        <>
+                            <Helmet>
+                                <title>{routes.profile.title}</title>
+                                <meta
+                                    name="description"
+                                    content="LLU Description"
+                                />
+                            </Helmet>
+                            <Profile />
+                        </>
+                    }
+                />
+                <Route
+                    path={routes.facilityView.path}
+                    element={
+                        <>
+                            <Helmet>
+                                <title>{routes.facilityView.title}</title>
+                                <meta
+                                    name="description"
+                                    content="LLU Description"
+                                />
+                            </Helmet>
+                            <FacilityView />
+                        </>
+                    }
+                />
+            </Route>
+        </Route>
+    )
 );
 
 const AppRouter = () => {
-  return (
-    <>
-      <RouterProvider router={router} />
-    </>
-  );
+    return (
+        <>
+            <RouterProvider router={router} />
+        </>
+    );
 };
 
 export default AppRouter;
